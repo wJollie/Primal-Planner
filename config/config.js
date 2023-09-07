d7f574ff00e2d4da779e9287ebaaf63a7e717607
MartinezN2014 <2014nmartinez@gmail.com>
2023-09-06 21:14:05 -0400

    added heroku & localstorage

--- config/config.js
+++ config/config.js
@@ -1,1 +1,24 @@
 // Database and other configuration settings
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+//JAWSDB shouldnt work until launched to heroku and jaws is installed
+//It's just here for later - Nelson
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
